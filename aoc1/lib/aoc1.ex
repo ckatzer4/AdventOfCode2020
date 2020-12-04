ff4ddaf9b74ff8b63d0a1a1fdc7b75719a182bea
Charles Katzer <cpkatzer@gmail.com>
2020-12-04 09:22:39 +0000

    auto-format day1

--- aoc1/lib/aoc1.ex
+++ aoc1/lib/aoc1.ex
@@ -18,34 +18,37 @@ defmodule Aoc1 do
 
   def parse do
     {:ok, text} = File.read("input")
-    ints = String.split(text, "\n")
-           |> Enum.filter(fn(s) -> String.length(s)>1 end)
-           |> Enum.map(&String.to_integer/1)
+
+    ints =
+      String.split(text, "\n")
+      |> Enum.filter(fn s -> String.length(s) > 1 end)
+      |> Enum.map(&String.to_integer/1)
+
     # ints = for n <- String.split(text,"\n"), do: String.to_integer(n)
     ints
   end
 
   def part1(ints) do
-    pairs = for p <- RC.comb(2,ints), do: p
-    pair = Enum.filter(pairs, fn(p) -> Enum.sum(p) == 2020 end)
+    pairs = for p <- RC.comb(2, ints), do: p
+    pair = Enum.filter(pairs, fn p -> Enum.sum(p) == 2020 end)
     [h, t] = hd(pair)
-    h*t
+    h * t
   end
 
   def part2(ints) do
-    triples = for t <- RC.comb(3,ints), do: t
-    triple = Enum.filter(triples, fn(p) -> Enum.sum(p) == 2020 end)
+    triples = for t <- RC.comb(3, ints), do: t
+    triple = Enum.filter(triples, fn p -> Enum.sum(p) == 2020 end)
     [p1, p2, p3] = hd(triple)
-    p1*p2*p3
+    p1 * p2 * p3
   end
-
 end
 
 defmodule RC do
   def comb(0, _), do: [[]]
   def comb(_, []), do: []
-  def comb(m, [h|t]) do
-    (for l <- comb(m-1, t), do: [h|l]) ++ comb(m, t)
+
+  def comb(m, [h | t]) do
+    for(l <- comb(m - 1, t), do: [h | l]) ++ comb(m, t)
   end
 end
 
--- aoc1/test/aoc1_test.exs
+++ aoc1/test/aoc1_test.exs
@@ -14,18 +14,17 @@ defmodule Aoc1Test do
 299
 675
 1456"
-    ints = for n <- String.split(text,"\n"), do: String.to_integer(n)
+    ints = for n <- String.split(text, "\n"), do: String.to_integer(n)
     assert ints == [1721, 979, 366, 299, 675, 1456]
   end
 
   test "calc" do
     ints = [1721, 979, 366, 299, 675, 1456]
-    pairs = for p <- RC.comb(2,ints), do: p
+    pairs = for p <- RC.comb(2, ints), do: p
     assert length(pairs) == 15
-    pair = Enum.filter(pairs, fn(p) -> Enum.sum(p) == 2020 end)
+    pair = Enum.filter(pairs, fn p -> Enum.sum(p) == 2020 end)
     assert length(pair) == 1
     [h, t] = hd(pair)
-    assert h*t == 514579
+    assert h * t == 514_579
   end
-    
 end
