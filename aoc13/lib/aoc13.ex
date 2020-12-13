9b939dbc65c3291b3031130edc61665e1f71a49a
Charles Katzer <cpkatzer@gmail.com>
2020-12-13 21:31:26 +0000

    Documentation and formatting improvements

--- aoc13/lib/aoc13.ex
+++ aoc13/lib/aoc13.ex
@@ -18,43 +18,47 @@ defmodule Aoc13 do
 
   def parse(text) do
     # first line is starting timestamp
-    # second line is available buses (igmore x)
+    # second line is available buses
     lines = String.split(text, "\n")
-    start = Enum.at(lines,0) |> String.to_integer()
-    buses = 
-      Enum.at(lines, 1)
+    start = Enum.at(lines, 0) |> String.to_integer()
+    buses = Enum.at(lines, 1)
     {start, buses}
   end
 
   def part1({start, buses}) do
-    buses = String.split(buses, ",")
+    buses =
+      String.split(buses, ",")
       |> Stream.filter(fn s -> s != "x" end)
       |> Enum.map(fn s -> String.to_integer(s) end)
-    {time, bus} = Enum.map(buses, fn bus ->
-      Stream.iterate(start, fn t -> t+1 end)
-      |> Stream.filter(&(rem(&1, bus) == 0))
-      |> Enum.at(0)
-    end)
-    |> Enum.zip(buses)
-    |> Enum.min_by(fn {time, _bus} -> time end)
-    (time-start)*bus
+
+    {time, bus} =
+      Enum.map(buses, fn bus ->
+        Stream.iterate(start, fn t -> t + 1 end)
+        |> Stream.filter(&(rem(&1, bus) == 0))
+        |> Enum.at(0)
+      end)
+      |> Enum.zip(buses)
+      |> Enum.min_by(fn {time, _bus} -> time end)
+
+    (time - start) * bus
   end
 
   def part2({_t, buses}) do
-    {buses, offsets} = String.split(buses, ",")
-    |> Enum.with_index()
-    |> Enum.filter(fn {bus, _} -> bus != "x" end)
-    |> Enum.map(fn {bus,idx} -> {String.to_integer(bus),idx} end)
-    # calc offset:
-    # smallest positive number such that bus will arrive
-    # 'idx' minutes later
-    # sometimes 'idx'>'bus', so we can do a terrible modulo hack
-    |> Enum.map(fn {bus,idx} -> {bus,rem(10*bus-idx,bus)} end)
-    |> Enum.unzip()
+    {buses, offsets} =
+      String.split(buses, ",")
+      |> Enum.with_index()
+      |> Enum.filter(fn {bus, _} -> bus != "x" end)
+      |> Enum.map(fn {bus, idx} -> {String.to_integer(bus), idx} end)
+      # calc offset:
+      # smallest positive number such that bus will arrive
+      # 'idx' minutes later
+      # sometimes 'idx'>'bus', so we can do a terrible modulo hack
+      |> Enum.map(fn {bus, idx} -> {bus, rem(10 * bus - idx, bus)} end)
+      |> Enum.unzip()
+
     # |> IO.inspect()
     CRT.find_val(offsets, buses)
   end
-
 end
 
 defmodule CRT do
@@ -64,17 +68,29 @@ defmodule CRT do
 
   @doc """
   Solves the system of equations where:
-    X mod moduli[0] = remainders[0]
-    X mod moduli[1] = remainders[1]
-    ...
-    X mod moduli[n-1] = remainders[n-1]
-    X mod moduli[n] = remainders[n]
+    * X mod moduli[0] = remainders[0]
+    * X mod moduli[1] = remainders[1]
+    * ...
+    * X mod moduli[n-1] = remainders[n-1]
+    * X mod moduli[n] = remainders[n]
+
+  ## Examples
+
+      iex> # Find X for:
+      iex> #  X mod 3 = 0
+      iex> #  X mod 4 = 3
+      iex> #  X mod 5 = 4
+      iex> CRT.find_val([0,3,4],[3,4,5])
+      39
+
   """
   def find_val(remainders, moduli) do
     # sort remainders and moduli by largest to smallest mod
-    {moduli, remainders} = Enum.zip(moduli, remainders)
-    |> Enum.sort_by(fn {m, _} -> m end, &>=/2)
-    |> Enum.unzip()
+    {moduli, remainders} =
+      Enum.zip(moduli, remainders)
+      |> Enum.sort_by(fn {m, _} -> m end, &>=/2)
+      |> Enum.unzip()
+
     start = hd(remainders)
     step = hd(moduli)
     inner(tl(remainders), tl(moduli), step, start)
@@ -84,20 +100,23 @@ defmodule CRT do
   Run each stage of the sieve.
 
   First stage finds a value that solves the first rwo equations.
-  Second stage finds a value that solves the first three; 
-  Third for four, etc.
+  Each subsequent iteration finds a solution that includes the next equation.
   """
+  def inner(remainders, moduli, step, result)
+
   def inner([], [], _step, result) do
     result
   end
 
   def inner([r | remainders], [mod | moduli], step, result) do
     # starting from result, increment by step and test
-    result = Stream.iterate(result, &(&1+step))
-    # |> Stream.map(&IO.inspect/1)
-    |> Stream.filter(fn t -> rem(t,mod) == r end)
-    # take the first result that passes the test
-    |> Enum.at(0)
-    inner(remainders, moduli, step*mod, result)
+    result =
+      Stream.iterate(result, &(&1 + step))
+      # |> Stream.map(&IO.inspect/1)
+      |> Stream.filter(fn t -> rem(t, mod) == r end)
+      # take the first result that passes the test
+      |> Enum.at(0)
+
+    inner(remainders, moduli, step * mod, result)
   end
 end
--- aoc13/test/aoc13_test.exs
+++ aoc13/test/aoc13_test.exs
@@ -17,19 +17,18 @@ defmodule Aoc13Test do
     text = "939
 7,13,x,x,59,x,31,19"
     p = Aoc13.parse(text)
-    assert Aoc13.part2(p) == 1068781
-    assert Aoc13.part2({0,"17,x,13,19"}) == 3417
-    assert Aoc13.part2({0,"67,7,59,61"}) == 754018
-    assert Aoc13.part2({0,"67,x,7,59,61"}) == 779210
-    assert Aoc13.part2({0,"67,7,x,59,61"}) == 1261476
-    assert Aoc13.part2({0,"1789,37,47,1889"}) == 1202161486
+    assert Aoc13.part2(p) == 1_068_781
+    assert Aoc13.part2({0, "17,x,13,19"}) == 3417
+    assert Aoc13.part2({0, "67,7,59,61"}) == 754_018
+    assert Aoc13.part2({0, "67,x,7,59,61"}) == 779_210
+    assert Aoc13.part2({0, "67,7,x,59,61"}) == 1_261_476
+    assert Aoc13.part2({0, "1789,37,47,1889"}) == 1_202_161_486
   end
 
   test "CRT" do
     # from Wikipedia
-    assert CRT.find_val([0,3,4],[3,4,5]) == 39
+    assert CRT.find_val([0, 3, 4], [3, 4, 5]) == 39
     # from Brilliant
-    assert CRT.find_val([1,4,6],[3,5,7]) == 34
+    assert CRT.find_val([1, 4, 6], [3, 5, 7]) == 34
   end
-    
 end
